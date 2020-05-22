260873a5abeb8cf8c163420b69624152029341df
girvel <widauka@ya.ru>
2020-05-22 21:01:22 +0500

    Realize deleting activities by index by very dirty code

--- organizer.py
+++ organizer.py
@@ -16,12 +16,21 @@ activities = []  # activity is (name, starting time, ending time)
 if __name__ == '__main__':
     print('Welcome to organizer, master')
     while True:
-        print(*(f'{tts(a[1])}\t{tts(a[2])}\t{a[0]}' for a in activities), sep='\n')
+        print(*(f' {i + 1}\t{tts(a[1])}\t{tts(a[2])}\t{a[0]}' for i, a in enumerate(activities)), sep='\n')
         action = input(':')
         if action in ('q', 'quit'):
             quit()
         elif action in ('e', 'end'):
             activities[-1][2] = time()
+        elif action.startswith('d') or action.startswith('delete'):
+            try:
+                index = int(action.split(' ')[1]) - 1
+            except:
+                index = len(activities) - 1
+            try:
+                activities = activities[:index] + activities[index + 1:]
+            except:
+                print('Wrong input')
         else:
             activities.append([action, time(), time()])
         clear()
