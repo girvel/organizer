404f61c58213d7b719b8a7494042a3089cbec67e
girvel <widauka@ya.ru>
2020-05-22 21:25:33 +0500

    Improve interface

--- organizer.py
+++ organizer.py
@@ -14,11 +14,31 @@ starting_time = time()
 activities = []  # activity is (name, starting time, ending time)
 
 if __name__ == '__main__':
+    clear()
     print('Welcome to organizer, master')
+    input()
     while True:
+        clear()
+        print(f'Started at {tts(starting_time)}\t\tNow is {tts(time())}\t\tSpent {tts(time() - starting_time - 18000)}')
         print(*(f' {i + 1}\t{tts(a[1])}\t{tts(a[2])}\t{a[0]}' for i, a in enumerate(activities)), sep='\n')
+        print()
+        total_time = time() - starting_time
+        parts = dict()
+        for a in activities:
+            if a[0] not in parts:
+                parts[a[0]] = 0
+            parts[a[0]] += a[2] - a[1]
+        sum = 0
+        for k, v in parts.items():
+            sum += v
+            v = round(v / total_time * 100)
+            print(f'{v}%\t{k}')
+        print(f'{round((1 - sum / total_time) * 100)}%\tDoing nothing')
+        print()
         action = input(':')
-        if action in ('q', 'quit'):
+        if not action:
+            continue
+        elif action in ('q', 'quit'):
             quit()
         elif action in ('e', 'end'):
             activities[-1][2] = time()
@@ -31,21 +51,5 @@ if __name__ == '__main__':
                 activities = activities[:index] + activities[index + 1:]
             except:
                 print('Wrong input')
-        elif action in ('s', 'stats', 'statistics'):
-            print()
-            total_time = time() - starting_time
-            parts = dict()
-            for a in activities:
-                if a[0] not in parts:
-                    parts[a[0]] = 0
-                parts[a[0]] += a[2] - a[1]
-            sum = 0
-            for k, v in parts.items():
-                sum += v
-                v = round(v / total_time * 100)
-                print(f'{v}%\t{k}')
-            print(f'{round((1 - sum / total_time) * 100)}%\tDoing nothing')
-            input()
         else:
             activities.append([action, time(), time()])
-        clear()
